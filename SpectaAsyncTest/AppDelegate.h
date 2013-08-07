665a6a541de7fee97ef5c1883bc2eae84a386af7
Mark DiFranco <mark.difranco@thalmic.com>
2013-08-07 12:33:44 -0400

    Initial Commit

---
+++ SpectaAsyncTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpectaAsyncTest
+//
+//  Created by Mark DiFranco on 2013-08-07.
+//  Copyright (c) 2013 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpectaAsyncTestTests/SpectaAsyncTestTests.m
@@ -0,0 +1,34 @@
+//
+//  SpectaAsyncTestTests.m
+//  SpectaAsyncTestTests
+//
+//  Created by Mark DiFranco on 2013-08-07.
+//  Copyright (c) 2013 Test. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpectaAsyncTestTests : XCTestCase
+
+@end
+
+@implementation SpectaAsyncTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
